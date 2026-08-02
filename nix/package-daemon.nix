16f7ef66b894dc10267437ae91304a3e214299fa
jytsma-wq <jytsma@gmail.com>
2026-08-03 03:06:16 +0400

    fix: sync final Nix dependency hash

--- nix/package-daemon.nix
+++ nix/package-daemon.nix
@@ -43,7 +43,7 @@ let
   # `nix build .#daemon` will fail with the expected hash printed; copy
   # that into `pnpmDepsHash` below. Bump it whenever pnpm-lock.yaml
   # changes.
-  pnpmDepsHash = "sha256-hUjNi0pzhLFkTZBkrwgqBZCU7N9q5rbPR9DEbwiijeg=";
+  pnpmDepsHash = "sha256-hWapVZCUJ22QcrjzAm7gZvtbFxq60Vc04TozS+dtJkY=";
   # pnpmDepsHash = lib.fakeHash;
 in
   stdenv.mkDerivation (finalAttrs: {
